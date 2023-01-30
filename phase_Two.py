53cfd2ec0ff639d0d87c9997bbbf1fbfac502d3e
Dhruvgiri Goswami <53379413+DhruvgiriGoswami@users.noreply.github.com>
2023-01-31 01:33:53 +0530

    Phase 2 completed. changed code completely and also made it way better by decreasing loops and no of variables used.
    Improvement required -
    1. Different colors for new lines of each points.

--- phase_Two.py
+++ phase_Two.py
@@ -1,6 +1,5 @@
 
 
-
 import matplotlib.pyplot as plt
 import random
 import math
@@ -8,13 +7,15 @@ import math
 
 x = []
 y = []
-closest_3_points = []
+
+
 dist = []
-index = []
-chunk_index = []
-close_point_index = []
 unsorted_dist = []
+closest_4_Points = []
+color = ['b','g','r','c','m',y,'k','w','b','g']
+
 tot_points = 10
+
 
 #taking random inputs from machine for co-ordinates.
 for i in range(1,tot_points+1,1):                                     #1st LOOP
@@ -32,78 +33,34 @@ plt.scatter(x, y, c="blue")
 
 
 for i in range(0,tot_points,1):                                      #2nd LOOP
-
+     dist.clear()
+     closest_4_Points.clear()
+     unsorted_dist.clear()
      for j in range(0,tot_points,1):
-          #print("x1 - ",x[i],"|y1 - ", y[i],"|x2 - ", x[j],"|y2 - ",  y[j])
-          dist_calc = math.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2)
-          print("distance of point ",i+1," from ", j+1, " - ", dist_calc)
 
-          #all the distances of current point to all other points stored in list 'dist'.
-          dist.append(dist_calc)
+         #print("x1 - ",x[i],"|y1 - ", y[i],"|x2 - ", x[j],"|y2 - ",  y[j])
+         dist_calc = math.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2)
+         print("distance of point ",i+1," from ", j+1, " - ", dist_calc)
 
-print("list of distances - ",dist)
-print("no of elements - ", len(dist))
+         #all the distances of current point to all other points stored in list 'dist'.
+         dist.append(dist_calc)
+         unsorted_dist = dist.copy()
 
-#take 3 closest distances to another points from current_working_point
-unsorted_dist = dist.copy()
-dist.sort()      #sorting the list to get the lowest values first in list.
-
-
-#here we have the sorted and unsorted list both of distances of each element
-#we have to divide the unsorted list into 10-10 seperate lists or elements and
-#then have next functions run on these consecutive lists/funcs.
-#sorted list will be useless right now
-
-for i in range(0, len(unsorted_dist), 10):
-
-    chunk = unsorted_dist[i:i + 10]
-    print("chunk ",int((i+10)/10)," - ", chunk)
+     dist.sort()
 
 
 
-#------------------------------------------------------------------#
+     for k in range(0,4):
+         closest_4_Points.append(dist[k])
+         #print("dist list - ",dist)
+         #print("list closest 4 points - ",closest_4_Points)
+         index = unsorted_dist.index(closest_4_Points[k])
+         print(index)
+         plt.plot([x[i],x[index]], [y[i],y[index]])
 
-for i in range(0, tot_points):                                          #3rd LOOP
-    for j in range(0, 3):
-     #appending distance from current_working_point to first 3 elements in the list 'closest_3_points'.
-        closest_3_points.append(chunk[j])
-
-#print("\nDistance from CWP to closest points - ",closest_3_points)
-
-
-
-#we have all the distances now and we can sort the 3 best distances too
-#but now we have to find the index of the 3 best distances in the unsorted list
-#so that we can come to know that which point is the closest from current_working_point.
+     print("closest point distances for ",i," - ",closest_4_Points)
 
 
-for i in range(0, tot_points):                                          #4th LOOP
-    for j in range(0, 3):
-
-     #adding index of closest points to "index".
-
-        close_point_index = unsorted_dist.index(closest_3_points[i])
-        close_point_chunk_index = unsorted_dist.index(closest_3_points)
-         #print(i,"th closest point index - ",close_point_index)
-        index.append(close_point_index)
-        chunk_index.append(close_point_chunk_index)
-
-     #we have now got the index of the closest points from original list.
-#print("Index of the closest 3 points in array is - ", index)
-
-
-#plotting lines to 3 closest points from current_working_point.
-
-
-#line 1
-for i in range(0,tot_points):                               #5th LOOP
-    for j in range(0, 3):
-         a = chunk_index[j]
-         plt.plot([x[i],x[a+1]], [y[i],y[a+1]])
-
-
-# To show the plot
+print("\nno of elements - ", len(dist),"\n")
 
 plt.show()
-
-#GG
